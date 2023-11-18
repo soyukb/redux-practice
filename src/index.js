01a6fc3f57c066cbcb1cbc4e60ca6434c7cf761a
soyukb <soyukoba@gmail.com>
2023-11-18 17:44:38 +0900

    コメントを入れる

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const increment = () => {
+  return {
+    type : "INCREMENT"
+  }
+}
+const decrement = () => {
+  return {
+    type : "DECREMENT"
+  }
+}
+
+const counterReducer = (state = 0, action) => {
+    switch (action.type){
+      case "INCREMENT":
+        return state + 1;
+        case "DECREMENT":
+          return state - 1;
+    }
+}
+
+let store = createStore(counterReducer)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +37,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
